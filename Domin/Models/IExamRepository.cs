1873325eff3c6dc9a3b7b0bb6dec74c295257f91
Hessamsd <hasansadeghi.pdev@gmail.com>
2024-12-01 17:50:00 +0330

    Implementing InterfaceExamRepository

---
+++ Domin/Models/IExamRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Domin.Models
+{
+    public interface IExamRepository
+    {
+        Task<Exam> GetExamByIdAsync(int id);
+        Task<List<Exam>> GetAllExamAsync();
+        Task<List<Student>> GetStudentsWhoCompletedPartiallyAsync(int examId);
+        Task<List<Student>> GetStudentsWhoDidNotTakeExamAsync(int groupId);
+    }
+}
