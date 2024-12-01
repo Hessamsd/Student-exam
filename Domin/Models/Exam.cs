e564e96be349f835bd584abb231f4e192187d388
Hessamsd <hasansadeghi.pdev@gmail.com>
2024-12-01 17:20:09 +0330

    Add project files.

---
+++ Domin/Models/Exam.cs
@@ -0,0 +1,14 @@
+﻿namespace Domin.Models
+{
+    public class Exam
+    {
+        public int ExamId { get; set; }
+        public string ExamName { get; set; }
+        public DateTime StartDate { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public int GroupId { get; set; }
+
+        //public ICollection<Questions>
+    }
+}
