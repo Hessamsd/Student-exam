01aa177406d9a01d69caf254c620351cc3e9a52e
Hessamsd <hasansadeghi.pdev@gmail.com>
2024-12-01 18:06:28 +0330

    Creating Context (Database)

---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructur
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Group> Groups { get; set; }
+
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
