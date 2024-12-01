be3b8e1de8c20dbf35387e1cfd7f8d182411e5a0
Hessamsd <hasansadeghi.pdev@gmail.com>
2024-12-01 19:41:54 +0330

    Implementing IApplication in the Application layer

---
+++ Application/IApplication.cs
@@ -0,0 +1,12 @@
+﻿using Domin.Models;
+
+namespace Application
+{
+    public interface IApplication
+    {
+        Task<Exam> GetExamByIdAsync(int examId);
+        Task<Student> GetStudentByIdAsync(int studentId);
+        Task<bool> SubmitAnswersAsync(int examId, int studentId, Dictionary<int, List<int>> answers);
+
+    }
+}
---
+++ Student exam/Pages/Exams.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Student_exam.Pages
+{
+    public class ExamsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
