c71acba5b80c7249de930d4f6dc14f4cc29d810c
Hessamsd <hasansadeghi.pdev@gmail.com>
2024-12-01 19:31:29 +0330

    ExamRepository (Repository implementation)

---
+++ Repository/Repository/ExamRepository.cs
@@ -0,0 +1,44 @@
+﻿using Domin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructur.Repository
+{
+    public class ExamRepository : IExamRepository
+    {
+
+        private AppDbContext _context;
+
+        public ExamRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Exam>> GetAllExamAsync()
+        {
+            return await _context.Exams.ToListAsync();
+        }
+
+        public async Task<Exam> GetExamByIdAsync(int id)
+        {
+            return await _context.Exams.Include(e => e.Questions).
+                ThenInclude(q => q.Options).
+                FirstOrDefaultAsync(e => e.ExamId == id);
+        }
+
+        public async Task<List<Student>> GetStudentsWhoCompletedPartiallyAsync(int examId)
+        {
+            var exam = await GetExamByIdAsync(examId);
+            var students = await _context.Students.Where(x => x.Answers
+            .Any(c => c.Question.ExamId == examId && c.AnswerDate > exam.StartDate 
+            && c.AnswerDate < exam.StartDate
+            .Add(exam.Duration))).ToListAsync();
+            return students;
+        }
+
+        public async Task<List<Student>> GetStudentsWhoDidNotTakeExamAsync(int groupId)
+        {
+            return await _context.Students
+                  .Where(d => !d.Answers.Any(a => a.Question.ExamId == groupId)).ToListAsync();   
+        }
+    }
+}
